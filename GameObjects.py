f558adac90ac5c25a85c70eaa8b2224f94e4e353
profmori <85837131+profmori@users.noreply.github.com>
2024-05-28 22:55:10 +0100

    Travel Code Added (Game Mechanics)
    Code  done on android added
    Implements the main game mechanics
    Needs to be commented and tidied up
    At the point now where 'Game Objects' should have their own files

--- GameObjects.py
+++ GameObjects.py
@@ -1,3 +1,7 @@
+from copy import copy
+from random import sample
+
+
 class Action:
     # Class for holding the different actions and logic for playing the game
     def __init__(self, name, move_dist=0, strike_min=-1, strike_range=0, end_stance='same', begin_stance='any',
@@ -66,12 +70,14 @@ class Board:
 
     def run_turn(self):
         # Function to run a single 2 card turn
+        self.samurai_1.choose_actions()
+        self.samurai_2.choose_actions()
+
         for action_num in range(2):
             # Iterate through both actions being played
-            s1_action = self.samurai_1.played_actions[action_num]
-            s2_action = self.samurai_2.played_actions[action_num]
+            s1_action = copy(self.samurai_1.played_actions[0])
+            s2_action = copy(self.samurai_2.played_actions[0])
             # Get the relevant action for this stage of the turn
-
             priorities = {s1_action.priority, s2_action.priority}
             # Make a set of the priorities - removes duplicates & orders the values
             for curr_priority in priorities:
@@ -85,41 +91,46 @@ class Board:
                     # If there is only one of the two samurai acting
                     if s1_act:
                         # If it is samurai 1
-                        self.samurai_1.perform_action(action_num)
+                        self.samurai_1.perform_action(self.samurai_2, self.board_size)
                         # Perform the action of samurai 1
                     else:
                         # If it is samurai 2
-                        self.samurai_2.perform_action(action_num)
+                        self.samurai_2.perform_action(self.samurai_1, self.board_size)
                         # Perform the action of samurai 2
                 else:
                     # If the two actions are the same priority (which can only be both true)
                     if self.samurai_1.stance == self.samurai_2.stance or curr_priority > 3:
                         # If the samurai are in the same stance, or combat is ocurring
-                        self.act_simultaneously(action_num)
+                        self.act_simultaneously()
                         # Enact their moves simultaneously
                     elif self.samurai_1.stance == 'heaven':
                         # If samurai 1 is in heaven stance, they act first
-                        self.samurai_1.perform_action(action_num, self.samurai_2)
-                        self.samurai_2.perform_action(action_num, self.samurai_1)
+                        self.samurai_1.perform_action(self.samurai_2, self.board_size)
+                        self.samurai_2.perform_action(self.samurai_1, self.board_size)
                     else:
                         # Otherwise samurai 2 is in heaven stance, and they act first
-                        self.samurai_2.perform_action(action_num, self.samurai_1)
-                        self.samurai_1.perform_action(action_num, self.samurai_2)
+                        self.samurai_2.perform_action(self.samurai_1, self.board_size)
+                        self.samurai_1.perform_action(self.samurai_1, self.board_size)
 
-            if self.samurai_1.attacked ^ self.samurai_2.attacked:
+            if self.samurai_1.attacking ^ self.samurai_2.attacking:
                 # If only one of the samurai attacked this turn (logical XOR)
-                self.samurai_1.health -= self.samurai_2.attacked
-                self.samurai_2.health -= self.samurai_1.attacked
+                self.samurai_1.health -= self.samurai_2.attacking
+                self.samurai_2.health -= self.samurai_1.attacking
                 # Both samurai take damage depending on whether their opponent attacked
                 # This implicitly translates the booleans into integers
-
-            self.samurai_1.attacked, self.samurai_2.attacked = False, False
+            self.samurai_1.attacking, self.samurai_2.attacking = False, False
             # Reset the attack flag on both samurai
+            self.samurai_1.counter_attacking, self.samurai_2.counter_attacking = False, False
+            # Get rid of the counter attacking variable after checking so a samurai doesn't become invincible
+            self.display_board()
+            # Show the board after each action
+            if self.samurai_1.health == 0 or self.samurai_2.health == 0:
+                break
 
-    def act_simultaneously(self, action_num):
+    def act_simultaneously(self):
         # Function to allow two samurai actions to be performed simultaneously
-        self.samurai_1.perform_action(action_num, None)
-        self.samurai_2.perform_action(action_num, None)
+        self.samurai_1.perform_action(self.samurai_2, self.board_size, True)
+        self.samurai_2.perform_action(self.samurai_1, self.board_size, True)
         # Perform actions as normal, ignoring opponent position & simultaneous factor
         while self.samurai_1.position > (self.board_size - self.samurai_2.position - 1):
             # If the samurai have tried to move past one another
@@ -127,15 +138,12 @@ class Board:
             self.samurai_2.position -= 1
             # Move both back one space and check again
 
-        if (self.samurai_1.counter_attack and self.samurai_2.attacked
-                or self.samurai_2.counter_attack and self.samurai_1.attacked):
+        if (self.samurai_1.counter_attacking and self.samurai_2.attacking
+                or self.samurai_2.counter_attacking and self.samurai_1.attacking):
             # If one character is attacking and the other is counter attacking
-            self.samurai_1.attacked = not self.samurai_1.attacked
-            self.samurai_2.attacked = not self.samurai_2.attacked
+            self.samurai_1.attacking = not self.samurai_1.attacking
+            self.samurai_2.attacking = not self.samurai_2.attacking
             # Invert the attack variables so the counter attacking samurai deals damage
-
-        self.samurai_1.counter_attack, self.samurai_2.counter_attack = False, False
-        # Get rid of the counter attacking variable after checking so a samurai doesn't become invincible
 
     def display_board(self, view=1):
         # Function to show the board, by default from samurai 1's perspective
@@ -146,15 +154,29 @@ class Board:
         # Get the samurai which should be displayed at the bottom of the view
         far_samurai = self.__getattribute__(f'samurai_{3 - view}')
         # Get the samurai which should be displayed at the top of the view
+        if close_samurai.health < 1:
+            colour_code = '\033[31m'
+        elif close_samurai.health < 2:
+            colour_code = '\033[34m'
+        else:
+            colour_code = '\033[32m'
+
         board_spaces[self.board_size - 1 - close_samurai.position][
-            1] = f'{close_samurai.player} ({close_samurai.stance})'
+            1] = f'{colour_code}{close_samurai.player} ({close_samurai.stance})'
         # Set index backwards from the last with the player's name and stance
-        board_spaces[far_samurai.position][0] = f'{far_samurai.player} ({far_samurai.stance})'
+        if far_samurai.health < 1:
+            colour_code = '\033[31m'
+        elif far_samurai.health < 2:
+            colour_code = '\033[34m'
+        else:
+            colour_code = '\033[32m'
+
+        board_spaces[far_samurai.position][0] = f'{colour_code}{far_samurai.player} ({far_samurai.stance})'
         # Set index forward from the first with the opponent's name and stance
 
         for position in board_spaces:
             # Iterate through the board spaces
-            print(f'{position[0].rjust(self.name_width)}|{position[1].ljust(self.name_width)}')
+            print(f'{position[0].rjust(self.name_width)}\033[0m|{position[1].ljust(self.name_width)}\033[0m')
             # Print the values in each position, spaced out so everything aligns down the middle
 
 
@@ -171,7 +193,7 @@ class Card:
             self.name = args[0].name
             # If there is only one action, copy its name
 
-        self.actions = args
+        self.actions = list(args)
         # Set the actions to be contained in the list
 
 
@@ -196,8 +218,61 @@ class Samurai:
         # Set as 0 for a basic game
         self.health = 2
         # Stores the samurai's health (2: healthy, 1: flipped, 0: dead)
-        self.attacked = False
+        self.attacking = False
         # Stores if the samurai is currently attacking
+        self.counter_attacking = False
+
+    def choose_actions(self):
+        self.played_cards = sample(self.hand, 2)
+        for card in self.played_cards:
+            action = sample(card.actions, 1)
+            self.played_actions += action
+            self.hand.remove(card)
+
+    def get_card(self, action):
+        for card in self.played_cards:
+            if action in card.actions:
+                return card
+        print(action.name)
+        return None
+
+    def perform_action(self, opponent, board_size, simultaneous=False):
+        action = self.played_actions[0]
+        if self.stance != action.begin_stance and action.begin_stance != 'any':
+            print(self.player, action.name, 'failed')
+        else:
+            print(self.player, action.name)
+            opp_pos = board_size - opponent.position - 1
+            self.position += action.movement
+            self.position = min(max(0, self.position), board_size - 1)
+            if not simultaneous and self.position > opp_pos:
+                self.position = opp_pos
+
+            if (self.position + action.strike_min) <= opp_pos <= (self.position + action.strike_max):
+                self.attacking = True
+                print('attacking')
+            elif action.strike_min > action.strike_max:
+                self.counter_attacking = True
+                print('counter attacking')
+
+            if action.end_stance != 'same':
+                if action.end_stance == 'change':
+                    end_stance = ['heaven', 'earth']
+                    end_stance.remove(self.stance)
+                    self.stance = end_stance[0]
+                else:
+                    self.stance = action.end_stance
+
+        if not action.single_use:
+            played_card = self.get_card(action)
+            if len(self.played_actions) == 2:
+                self.hand.append(played_card)
+            else:
+                if self.discard is not None:
+                    self.hand.append(self.discard)
+                self.discard = played_card
+
+        self.played_actions.pop(0)
 
     def return_actions(self):
         # Function to return all the available actions for the samurai
@@ -206,7 +281,7 @@ class Samurai:
         # Create an empty list
         for card in self.hand:
             # For each card in the samurai's hand
-            actions.append(card.actions)
+            actions += card.actions
             # Add all that cards actions to the action list
 
         return actions
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from GameObjects import Samurai, Board, Card, Action
+from copy import copy
+
+from GameObjects import Card, Action, Samurai, Board
 
 if __name__ == '__main__':
     basic_card_list = [Card(Action('high strike', begin_stance='heaven', strike_min=2)),
@@ -9,8 +11,10 @@ if __name__ == '__main__':
     # The list of basic cards for both players
 
     special_card_list = [
-        Card(Action('kesa strike', begin_stance='heaven', end_stance='earth', strike_min=0, strike_range=1, single_use=True)),
-        Card(Action('zan-tetsu strike', begin_stance='earth', end_stance='heaven', strike_min=2, strike_range=1, single_use=True)),
+        Card(Action('kesa strike', begin_stance='heaven', end_stance='earth', strike_min=0, strike_range=1,
+                    single_use=True)),
+        Card(Action('zan-tetsu strike', begin_stance='earth', end_stance='heaven', strike_min=2, strike_range=1,
+                    single_use=True)),
         Card(Action('counterattack', strike_min=0, strike_range=-1, single_use=True))]
     # The special attacks
 
@@ -18,8 +22,13 @@ if __name__ == '__main__':
     # Initialises a samurai called Jom
     p2 = Samurai('rajun')
     # Initialises a samurai called Rajun
-    p1.hand = basic_card_list
-    print(p1.return_actions())
+    p1.hand = copy(basic_card_list) + copy(special_card_list)
+    p2.hand = copy(basic_card_list) + copy(special_card_list)
     new_board = Board('basic', p1, p2)
     # Begins a game on the basic board with these players
     # No logic or 'game' implemented yet, simply prints the board
+    i = 0
+    while p1.health > 0 and p2.health > 0:
+        i += 1
+        print('Turn', i)
+        new_board.run_turn()
